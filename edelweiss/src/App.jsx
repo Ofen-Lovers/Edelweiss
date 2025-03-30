55c8ac87c6335447d0f92dd6dbe63b754b56a1f7
Jhon Lloyd F. Omblero <omblero.jhonlloyd.04@gmail.com>
2025-03-30 23:46:28 +0800

    Initial commit

---
+++ edelweiss/src/App.jsx
@@ -0,0 +1,19 @@
+import 'boostrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+<h1 className='text-center p-5' style={{fontSize:'70px', fontWeight:'bold'}}> Build a <span className='text-p'></span> </h1>
+<br/> By <span className='text-danger'>Edelweiss</span>
+    </>
+  )
+}
+
+export default App;
