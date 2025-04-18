299300ad30fdf684679669976f0e9e61ab3ddba5
Jhon Lloyd F. Omblero <omblero.jhonlloyd.04@gmail.com>
2025-04-18 15:45:39 +0800

    Update search drop result and it's functionality

--- edelweiss/src/components/selectDrop/select.jsx
+++ edelweiss/src/components/selectDrop/select.jsx
@@ -12,6 +12,10 @@ const Select = ({data, placeholder, icon}) => {
     const [selectedIndex, setSelectedIndex] = useState(0);
     const [selectedItem, setselectedItem] = useState(placeholder);
 
+    const [listData, setListData] = useState(data);
+    const [listData2, setListData2] = useState(data);
+
+
     const OpenSelect = () => {
         setisOpenSelect(!isOpenSelect);
     }
@@ -21,25 +25,38 @@ const Select = ({data, placeholder, icon}) => {
         setisOpenSelect(false);
         setselectedItem(name);
     }
+
+    const filterList = (e) => {
+        const keyword = e.target.value.toLowerCase();
+        const list = listData2.filter((item) => {
+            return item.toLowerCase().includes(keyword);
+
+        })
+
+        const list2 = list.filter((item,index) => list.indexOf(item)===index);
+
+        setListData(list2);  
+
+    }
     
 
     return (
         <ClickAwayListener onClickAway={()=>setisOpenSelect(false)}>
         <div className='selectDropWrapper cursor position-relative'>
             {icon}
-            <span className='openSelect' onClick={OpenSelect}>{selectedItem} <KeyboardArrowDownIcon className='arrow'/></span>
+            <span className='openSelect' onClick={OpenSelect}>{selectedItem.length>14 ? selectedItem.substr(0,11)+'...' : selectedItem} <KeyboardArrowDownIcon className='arrow'/></span>
             {
                 isOpenSelect===true && 
                 <div className='selectDrop'>
                 <div className='searchField'>
-                    <input type="text" placeholder='Search here...'/>  
+                    <input type="text" placeholder='Search here...' onChange={filterList}/>  
                 </div>
                 <ul className='searchResults'>
                 <li key={0} onClick={()=>closeSelect(0,placeholder)} className={`${selectedIndex===0 ? 'active' :''}`}>{placeholder}</li>
 
 
                     {
-                        data.map((item, index) => {
+                        listData.map((item, index) => {
                             return (
                                 <li key={index+1} onClick={()=>closeSelect(index+1,item)} className={`${selectedIndex===index+1 ? 'active' :''}`}>{item}</li>
                             )
