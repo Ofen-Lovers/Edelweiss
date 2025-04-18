5f7a158f103ae894e112db17a7b458e36a50e094
Jhon Lloyd F. Omblero <omblero.jhonlloyd.04@gmail.com>
2025-04-18 16:40:53 +0800

    Added dropdown in My Account menu

--- edelweiss/src/components/header/header.jsx
+++ edelweiss/src/components/header/header.jsx
@@ -6,12 +6,22 @@ import SearchIcon from "@mui/icons-material/Search";
 import Select from "../selectDrop/select";
 import axios from "axios";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
-import CompareArrowsOutlinedIcon from '@mui/icons-material/CompareArrowsOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
- 
+import CompareArrowsOutlinedIcon from "@mui/icons-material/CompareArrowsOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import Button from '@mui/material/Button';
+
 const Header = () => {
+
+  const [isOpenDropdown, setisOpenDropdown] = useState(false);
+
   const [categories, setcategories] = useState([
     "Stuff Toys",
     "Boquetes",
@@ -76,32 +86,55 @@ const Header = () => {
                   <Select
                     data={countryList}
                     placeholder={"Your Location"}
-                    icon={<LocationOnOutlinedIcon style={{color:'ff2b8e'}}/>}
+                    icon={
+                      <LocationOnOutlinedIcon style={{ color: "ff2b8e" }} />
+                    }
                   />
                 </div>
 
                 <ul className="list list-inline mb-0 headerTabs">
                   <li className="list-inline-item">
-                    <span><CompareArrowsOutlinedIcon className="compare-icon"/>
-                    <span className="badge rounded-circle">3</span>
-                    Compare</span>
+                    <span>
+                      <CompareArrowsOutlinedIcon className="compare-icon" />
+                      <span className="badge rounded-circle">3</span>
+                      Compare
+                    </span>
                   </li>
                   <li className="list-inline-item">
-                    <span><FavoriteBorderOutlinedIcon className="compare-icon"/>
-                    <span className="badge rounded-circle">4</span>
-                    Favorite</span>
+                    <span>
+                      <FavoriteBorderOutlinedIcon className="compare-icon" />
+                      <span className="badge rounded-circle">4</span>
+                      Favorite
+                    </span>
                   </li>
                   <li className="list-inline-item">
-                    <span><ShoppingCartOutlinedIcon className="compare-icon"/>
-                    <span className="badge rounded-circle">1</span>
-                    Cart</span>
+                    <span>
+                      <ShoppingCartOutlinedIcon className="compare-icon" />
+                      <span className="badge rounded-circle">1</span>
+                      Cart
+                    </span>
                   </li>
                   <li className="list-inline-item">
-                    <span><AccountCircleOutlinedIcon className="compare-icon"/>
-                    <span className="badge rounded-circle"></span>
-                    Account</span>
+                    <span onClick={() => setisOpenDropdown(!isOpenDropdown)}>
+                    <AccountCircleOutlinedIcon className="compare-icon" />
+                      <span className="badge rounded-circle"></span>
+                      Account
+                    </span>
+                    {
+                      isOpenDropdown !== false &&
+                      <ul className="dropdownMenu">
+                      <li><Button className="align-items-center"><AccountCircleIcon/>My Account</Button></li>
+                      <li><Button><DeliveryDiningIcon/>Order Tracking</Button></li>
+                      <li><Button><ConfirmationNumberIcon/>Voucher</Button></li>
+                      <li><Button><FavoriteIcon/>Favorite</Button></li>
+                      <li><Button><SettingsIcon/>Setting</Button></li>
+                      <li><Button><ExitToAppIcon/>Sign out</Button></li>
+                    </ul>
+
+                    }
+
                   </li>
-                </ul>
+                </ul> 
               </div>
             </div>
           </div>
