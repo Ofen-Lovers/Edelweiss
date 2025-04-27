e4fb34b88e77f40a9271a13cc862b15bedbb0a81
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-04-27 15:46:14 +0800

    edited Footer

---
+++ edelweiss/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './Footer.css';
+
+
+const Footer = () =>{
+    return(
+        <footer className="footer">
+            <p>Built with Edelweiss & Co. © 2025 All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
